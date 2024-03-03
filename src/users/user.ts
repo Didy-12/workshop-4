bc86bc7c4cdfc4e72916cb83ee3447a5b57ba75c
heddy <120495158+Didy-12@users.noreply.github.com>
2024-03-03 23:02:58 +0100

    Update user.ts

--- src/users/user.ts
+++ src/users/user.ts
@@ -1,6 +1,16 @@
-import bodyParser from "body-parser";
 import express from "express";
+import bodyParser from "body-parser";
 import { BASE_USER_PORT } from "../config";
+import {
+  generateRsaKeyPair,
+  exportPubKey,
+  rsaEncrypt,
+  createRandomSymmetricKey,
+  exportSymKey,
+  symEncrypt,
+} from "../crypto";
+
+
 
 export type SendMessageBody = {
   message: string;
@@ -12,8 +22,33 @@ export async function user(userId: number) {
   _user.use(express.json());
   _user.use(bodyParser.json());
 
-  // TODO implement the status route
-  // _user.get("/status", (req, res) => {});
+
+  // Variables to store the last received and last sent messages
+  let lastReceivedMessage: string | null = null;
+  let lastSentMessage: string | null = null;
+
+  // Route to receive messages
+  _user.post("/message", (req, res) => {
+    const { message }: { message: string } = req.body;
+    console.log(`User ${userId} received message: ${message}`);
+    lastReceivedMessage = message;
+    res.send("success");
+  });
+
+  // Route to retrieve the last received message
+  _user.get("/getLastReceivedMessage", (req, res) => {
+    res.json({ result: lastReceivedMessage });
+  });
+
+  // Route to retrieve the last sent message
+  _user.get("/getLastSentMessage", (req, res) => {
+    res.json({ result: lastSentMessage });
+  });
+
+  _user.get("/status", (req, res) => {
+    res.send("live");
+  });
+
 
   const server = _user.listen(BASE_USER_PORT + userId, () => {
     console.log(
@@ -23,3 +58,15 @@ export async function user(userId: number) {
 
   return server;
 }
+
+
+
+
+
+
+
+
+
+
+
+
