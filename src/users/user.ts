aaf023521a2e907d735816c0282ad4842e5ca473
heddy <120495158+Didy-12@users.noreply.github.com>
2024-03-04 20:10:54 +0100

    Update user.ts

--- src/users/user.ts
+++ src/users/user.ts
@@ -1,21 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { BASE_USER_PORT } from "../config";
+import { GetNodeRegistryBody,Node} from "../registry/registry";
+import axios from "axios";  
+import { BASE_USER_PORT, BASE_ONION_ROUTER_PORT, REGISTRY_PORT } from "../config";
 import {
-  generateRsaKeyPair,
-  exportPubKey,
-  rsaEncrypt,
   createRandomSymmetricKey,
-  exportSymKey,
   symEncrypt,
+  rsaEncrypt,
+  exportSymKey
 } from "../crypto";
-
 
 
 export type SendMessageBody = {
   message: string;
   destinationUserId: number;
 };
+
+interface RegistryResponse {
+  nodes: Node[];
+}
+
 
 export async function user(userId: number) {
   const _user = express();
@@ -26,6 +30,7 @@ export async function user(userId: number) {
   // Variables to store the last received and last sent messages
   let lastReceivedMessage: string | null = null;
   let lastSentMessage: string | null = null;
+  let lastCircuit: Node[] = [];
 
   // Route to receive messages
   _user.post("/message", (req, res) => {
@@ -48,6 +53,97 @@ export async function user(userId: number) {
   _user.get("/status", (req, res) => {
     res.send("live");
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Route pour obtenir le dernier circuit
+_user.get("/getLastCircuit", (req, res) => {
+  res.json({ status: 200, result: lastCircuit.map(node => node.nodeId) });
+});
+
+
+_user.post("/message", (req, res) => {
+  const message = req.body.message;
+
+  lastReceivedMessage = message;
+
+  console.log(`Received message: ${message}`);
+
+  // Send a success response
+  res.status(200).send("success");
+});
+
+_user.post("/sendMessage", async (req, res) => {
+  // Extract the message
+  const { message, destinationUserId } = req.body;
+
+  // Retrieve the list of available nodes in the network
+  const nodes = await fetch(`http://localhost:8080/getNodeRegistry`)//fetch the nodes list
+      .then((res) => res.json() as Promise<GetNodeRegistryBody>)
+      .then((body) => body.nodes);
+
+  // Create a circuit of 3 nodes from the list of available nodes (randomly)
+  let circuit: Node[] = [];
+  while (circuit.length < 3) { //creates a 3 nodes circuit from the nodes list
+    const randomNode = nodes[Math.floor(Math.random() * nodes.length)];
+    if (!circuit.includes(randomNode)) {
+      circuit.push(randomNode);
+    }
+  }
+
+  // Prepare the message to be sent
+  // The destination identifier is formatted with padding to reach a length of 10 characters.
+  let destination = `${BASE_USER_PORT + destinationUserId}`.padStart(10, "0");
+  let finalMessage = message;
+  // Generate a symmetric key for each node
+  for(const node of circuit) {
+    const symmetricKey = await createRandomSymmetricKey();
+    const symmetricKey64 = await exportSymKey(symmetricKey);
+    const encryptedMessage = await symEncrypt(symmetricKey, `${destination + finalMessage}`);
+    destination = `${BASE_ONION_ROUTER_PORT + node.nodeId}`.padStart(10, '0');
+    const encryptedSymKey = await rsaEncrypt(symmetricKey64, node.pubKey);
+    finalMessage = encryptedSymKey + encryptedMessage;
+  }
+
+  // Reverse the circuit to get the correct order of nodes
+  circuit.reverse();
+  lastCircuit = circuit;
+  lastSentMessage = message;
+  await fetch(`http://localhost:${BASE_ONION_ROUTER_PORT + circuit[0].nodeId}/message`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ message: finalMessage }),
+  });
+  res.status(200).send("success");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   const server = _user.listen(BASE_USER_PORT + userId, () => {
